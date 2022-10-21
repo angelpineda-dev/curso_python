6e070542c711b80dc41e8ed2299fe31f9515484d
Angel Pineda <angeljpa95@gmail.com>
2022-10-20 20:17:57 -0500

    variables y convencion de variables

---
+++ modulo1/1-variables.py
@@ -0,0 +1,12 @@
+# nombre = valor
+
+titulo_curso = ">_ Curso profesional de Python"
+print(titulo_curso)
+
+# Las variables pueden modificar su valor en tiempo de ejecucion
+titulo_curso = ">_ Curso Python"
+print(titulo_curso)
+
+# convencion de variables python
+nombre_completo = ">_ Angel Jesus Pineda Aguirre"
+print(nombre_completo)
