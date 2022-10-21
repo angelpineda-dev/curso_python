dd5d89e228988561274fcb1215c15d62928c8539
Angel Pineda <angeljpa95@gmail.com>
2022-10-20 21:52:35 -0500

    tipado dinamico

---
+++ modulo1/4-tipado.py
@@ -0,0 +1,12 @@
+# Tipado dinamico
+'''
+Es la posibilidad que tienen las variables de almacenar
+diferentes tipos de datos en diferentes momentos
+'''
+
+valor = "Angel"
+valor = 2
+valor = 3.1
+valor = True
+
+print(valor)
