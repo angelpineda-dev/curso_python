7932ec172f52106d83289282b66987a6c00b7815
Angel Pineda <angeljpa95@gmail.com>
2022-10-20 21:04:19 -0500

    constantes

---
+++ modulo1/2-constantes.py
@@ -0,0 +1,5 @@
+# Colocando la variable totalmente en mayusculas indicamos que es una constante
+# y solo la utilizaremos para lectura
+
+TITULO_CURSO = "Curso profesional de Python"
+print(TITULO_CURSO)
