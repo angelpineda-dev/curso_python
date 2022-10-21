ae2f4ff360e1fa71df920e0a6df7414b6209d78a
Angel Pineda <angeljpa95@gmail.com>
2022-10-20 22:09:54 -0500

    operadores relacionales

---
+++ modulo1/5-relacionales.py
@@ -0,0 +1,28 @@
+# Operadores relacionales
+
+'''
+- Nos permiten obtener un valor booleano (True o False)
+ > mayor que
+ < menor que
+ >= mayor o igual que
+ <= menor o igual que
+ == igual que
+ != diferente de
+'''
+
+numero_uno = 10
+numero_dos = 20
+
+resultado = numero_uno >= numero_dos
+print(resultado)
+
+resultado = numero_uno <= numero_dos
+print(resultado)
+
+resultado = numero_uno == numero_dos
+print(resultado)
+
+resultado = numero_uno != numero_dos
+print(resultado)
+
+
