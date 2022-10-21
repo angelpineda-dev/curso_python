4b64daf53441eed4ba5cbbf7b13d283abedfef4f
Angel Pineda <angeljpa95@gmail.com>
2022-10-20 21:24:46 -0500

    ÃPrincipales tipos de datos

---
+++ modulo1/3-tipos_datos.py
@@ -0,0 +1,28 @@
+# String
+titulo_curso = "Curso profesional de Python"
+print(titulo_curso)
+
+nombre_completo = 'Angel Juanito Rodriguez Martinez'
+print(nombre_completo)
+
+# Para crear un String con saltos de linea podemos utilizar triple comilla simple o doble.
+mensaje = """Te encuentras 
+en el curso: Profesional de Python.
+En CódigoFacilito
+"""
+print(mensaje)
+
+# Con los números podemos utilizar operadores aritmeticos (sumar, restar, multiplicar, dividir)
+# Int - nos permite trabajar con números enteros negativos o positivos.
+numero_uno = 10 * 2 // 3 
+print(numero_uno)
+
+# Float - permite trabajar con números reales, aquellos que poseen punto decimal y pueden ser positivos o negativos
+numero_dos = 3.14
+print(numero_dos)
+
+# Bool - unicamente almacena 2 valores, verdadero o falso.
+
+valor = True
+valor_dos = False
+print(valor, valor_dos)
