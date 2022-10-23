aae754e246e556cfeca3aeec11e217d9978f1736
Angel Pineda <angeljpa95@gmail.com>
2022-10-23 13:53:00 -0500

    convertir tipos de datos

---
+++ modulo1/9-convertir_tipo_datos.py
@@ -0,0 +1,13 @@
+nombre_completo = input('Ingresa tu nombre completo: ')  # str
+
+edad = int(input('Ingresa tu edad: '))
+
+altura = float(input('Ingresa tu altura (metros): '))
+
+autorizacion = input('Autozizas el programa? (si/no) ') == 'si'
+
+print(nombre_completo)
+print(edad)
+print(altura)
+print(autorizacion)
+
