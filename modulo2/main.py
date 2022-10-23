638cd7418d394e1ebcf95300a2fdbe492268ba08
Angel Pineda <angeljpa95@gmail.com>
2022-10-23 17:04:47 -0500

    introduccion a las listas

---
+++ modulo2/main.py
@@ -0,0 +1,15 @@
+# listas - son estructuras de datos. 
+# Un tipo de datos que nos permite manejar otro tipo de datos.
+# A cada valor dentro de la lista le corresponde un indice, comenzando por 0
+# Se aconseja crear listas que almacenen un solo tipo de valor
+
+# Existen 2 opciones para crear una lista
+lista = ['String', 10, 13.14, True] # list()
+lista_strings = list(("Eduardo", "Codigo", "Platano", "Curso"))
+
+lista_enteros = [-2, 3, 23, 0]
+lista_floats = [13.1416, -20.5, 90.2, 19.88]
+lista_booleanos = [True, True, (2 < 10), (4 > 3 and 10 != 11)]
+
+print(lista)
+print(lista_strings)
