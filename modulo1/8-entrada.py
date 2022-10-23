d8a202843216ab3d61023b23c48c23e29a9f6da0
Angel Pineda <angeljpa95@gmail.com>
2022-10-23 13:41:15 -0500

    Ãobtener datos del teclado

---
+++ modulo1/8-entrada.py
@@ -0,0 +1,10 @@
+'''
+Obtener valores que el usuario ingrese mediante el teclado
+con ayuda de la funcion "input(message)"
+esta funcion detiene la ejecución del programa y 
+regresa (retorna) la entrada del teclado
+'''
+
+nombre_completo = input('Ingresa tu nombre completo: ')  # str
+print(nombre_completo)
+print(type(nombre_completo))
