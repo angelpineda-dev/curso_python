b4a0bc91f71b8b48c9ccd989ae377d0483b51a6b
Angel Pineda <angeljpa95@gmail.com>
2022-10-23 14:00:49 -0500

    Ãmultiples variables

---
+++ modulo1/10-multiples-variables.py
@@ -0,0 +1,19 @@
+""" 
+En python la legibilidad cuenta, 
+por lo cual debemos tener en cuenta lo siguiente.
+
+- Declarar variables relacionadas al mismo contexto
+- No declarar más de 3 o 4 variables en una sola linea
+"""
+
+"""
+nombre = 'Angel'
+apellido = 'Rodriguez'
+titulo = "Mr."
+ """
+
+nombre, apellido, titulo = 'Angel', 'Rodriguez', 'Mr.'
+
+print(nombre)
+print(apellido)
+print(titulo)
