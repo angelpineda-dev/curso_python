62c4bf36701165ccf1ac7992a00f7f108b628d70
Angel Pineda <angeljpa95@gmail.com>
2022-10-23 13:32:44 -0500

    conocer tipo de dato

---
+++ modulo1/7-tipado.py
@@ -0,0 +1,16 @@
+'''
+Si queremos conocer que tipo de datos esta almacenando una variable
+podemos utilizar la funcion type(valor)
+'''
+valor = "Jaime"
+tipo = type(valor)
+print(tipo)
+
+valor = 2
+print(type(valor))
+
+valor = 3.14
+print(type(valor))
+
+valor = True
+print(type(valor))
