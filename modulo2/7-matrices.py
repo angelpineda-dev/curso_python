9d349ac235b66545e7cefd1800f48d59e490ba85
Angel Pineda <angeljpa95@gmail.com>
2022-10-26 16:04:30 -0500

    matrices con listas

---
+++ modulo2/7-matrices.py
@@ -0,0 +1,9 @@
+# Matrices
+
+columna_a = [10, 20, 50, 60]
+columna_b = [30, 40, 70, 80]
+
+matriz = [columna_a, columna_b] # 2 x 2
+
+# Imprimir el primer elemento de la segunda columna
+print(matriz[1][0])
