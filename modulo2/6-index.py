08e97183a5d3da03d87e1eb435a7d9c847a4366e
Angel Pineda <angeljpa95@gmail.com>
2022-10-26 15:45:50 -0500

    index de una lista

---
+++ modulo2/6-index.py
@@ -0,0 +1,13 @@
+# Index
+
+lista = [8, 90, 1, 5, 44, 132, 5, 60, 3, 4, 5]
+
+print(5 in lista)
+
+# lista.index() nos ayuda a conocer el indice de un elemento existente en una lista.
+print(lista.index(5))
+
+""" 
+  El metodo retornar la primera coincidencia que encuentre en la lista en caso
+  de existir mas elementos con el mismo valor.
+"""
