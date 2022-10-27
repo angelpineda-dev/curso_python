71cb3313150015dfc49caabc0048556a05cbe498
Angel Pineda <angeljpa95@gmail.com>
2022-10-26 22:25:13 -0500

    tuplas indices

---
+++ modulo3/1-indices.py
@@ -0,0 +1,15 @@
+# Indices
+
+#             0        1         2         3         4
+cursos = ('Python', 'Flask', 'Django', 'Rails', 'MongoDB')
+
+primer_curso = cursos[0]
+print(primer_curso)
+
+ultimo_curso = cursos[-1]
+print(ultimo_curso)
+
+# Sub tupla - funciona como las listas [start:end:skip]
+
+sub_tupla = cursos[0:3]
+print(sub_tupla)
