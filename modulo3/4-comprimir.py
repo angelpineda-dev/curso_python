101ef40ae024989b19395897db94dc8e9730cce0
Angel Pineda <angeljpa95@gmail.com>
2022-10-26 22:54:43 -0500

    tuplas compresion

---
+++ modulo3/4-comprimir.py
@@ -0,0 +1,16 @@
+# Comprimir tuplas
+
+# Los elementos restantes no son considerados para generar las tuplas de la comrpesion. 
+lista = [1, 2, 3, 4, 5, 6, 7]
+
+tupla = (10, 20, 30, 40, 50)
+
+lista_2 = [100, 200, 300, 400, 500, 600, 700]
+
+tupla_2 = (1000, 2000, 3000, 4000, 5000)
+
+
+compresion = zip(lista, tupla, lista_2, tupla_2) # -> zip
+resultado = tuple(compresion)
+
+print(resultado)
