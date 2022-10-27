238174b9a23c95e0e713eaea8aa7ed55eeeae9f9
Angel Pineda <angeljpa95@gmail.com>
2022-10-26 22:07:53 -0500

    introduccion tuplas

---
+++ modulo3/main.py
@@ -0,0 +1,13 @@
+# Tuplas
+# Son parecidas a las listas, por lo tanto nos permiten almacenar diferentes tipos de datos.
+# Las tuplas son inmutables a diferencia de las listas, no podremos modificarlas después de haberlas creado.
+
+# A cada uno de los elementos dentro de la tupla les corresponde un indice.
+
+# Ventajas: Son mas rapidas que las listas al obtener elementos, al ser inmutables python las almacena en un espacio en memoria especial para objetos de solo lectura.
+
+tupla = ('String', 10, 2.54, True, [1,2,3], (4,5,6))
+
+# print(type(tupla))
+
+print(tupla)
