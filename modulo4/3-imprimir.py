052e9abfbeb78de3614e5ad21855ddd826db533f
Angel Pineda <angeljpa95@gmail.com>
2022-11-01 15:17:19 -0600

    strings: funcion print

---
+++ modulo4/3-imprimir.py
@@ -0,0 +1,9 @@
+# Funcion print()
+
+nombre = 'Tomas Eduardo'
+apellido = 'Garcia'
+
+# Para evitar crear mas variables, podemos imprimir los valores directamente en la funcion print. 
+# La funcion print por default separa los valores con un espacio. Para cambiar esto podemos agregar el parametro sep='caracter'
+
+print(nombre, apellido, 'Perez', True, 1.89, sep=' >.< ')
