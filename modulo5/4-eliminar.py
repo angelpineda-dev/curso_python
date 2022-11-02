2b85d200cacf403562f9db8527c4f8483772d778
Angel Pineda <angeljpa95@gmail.com>
2022-11-02 12:09:55 -0600

    diccionarios: eliminar elementos

---
+++ modulo5/4-eliminar.py
@@ -0,0 +1,15 @@
+# Eliminar
+
+diccionario = {'a':1, 'b':2, 'c':3, 'd':4,}
+
+# Palabra reservada del
+del diccionario['a']
+print(diccionario)
+
+# metodo pop() - recibe el elemento a extraer del diccionario y retorna el valor removido.
+diccionario.pop('c')
+print(diccionario)
+
+# Eliminar todos los elementos con el metodo clear()
+diccionario.clear()
+print(diccionario)
