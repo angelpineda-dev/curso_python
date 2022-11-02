5a5c4ad71df9f4176a074cbd9f0ae64099890f08
Angel Pineda <angeljpa95@gmail.com>
2022-11-02 12:04:18 -0600

    diccionarios: llaves, items y valores

---
+++ modulo5/3-llaves_items_valores.py
@@ -0,0 +1,17 @@
+# Llaves, items y valores
+
+diccionario = {'a':1, 'b':2, 'c':3, 'd':4,}
+
+# Con la ayuda de los siguientes metodos, podremos obtener los diferentes valores dentro de tuplas, las cuales son inmutables y solo para lectura.
+
+# keys
+llaves = diccionario.keys()
+print(llaves)
+
+# values
+valores = diccionario.values()
+print(valores)
+
+# items
+objetos = diccionario.items()
+print(objetos)
